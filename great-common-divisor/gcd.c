dd09cba2eadac3deed37f919ff667917bb92f1ae
MD Sabbir Hossain <hossainmdbsabbir@gmail.com>
2026-01-16 15:35:55 +0600

    Great Common Divisor initial push

---
+++ great-common-divisor/gcd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, gcd;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    for(int i = 1; i <= num1 && i <= num2; i++) { 
+        if(num1 % i == 0 && num2 % i == 0) {
+            gcd = i;
+        }
+    }
+    
+    printf("GCD is: %d", gcd);
+    
+    return 0;
+}
